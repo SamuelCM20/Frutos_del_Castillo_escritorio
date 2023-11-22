d87ceab469a4bb033b718a3e2b33f48e2ea86711
Nicol-Beltran <111929809+Nicol-Beltran@users.noreply.github.com>
2023-11-21 20:39:13 -0500

    Se añade modelo categoria

---
+++ src/main/java/Modelo/Categorias.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author NICOL
+ */
+public class Categorias {
+    private int idCategoria;
+    private String nombre;
+    private String imagen;
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+    
+    
+}
