e7ac1024f9d6a95caa7be9faee5930b5fd4a396d
jd2698 <j.delgado2698@gmail.com>
2024-02-14 00:05:32 -0500

    "Actualización controlador reportes"

--- src/main/java/controladores/ControladorReportes.java
+++ src/main/java/controladores/ControladorReportes.java
@@ -5,6 +5,8 @@
 package controladores;
 
 import java.io.FileOutputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
@@ -15,6 +17,7 @@ import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -26,18 +29,22 @@ public class ControladorReportes {
 
     ControladorMesas objControladorMesas = new ControladorMesas();
     ControladorPedidos objControladorPedidos = new ControladorPedidos();
+    ControladorFacturas objControladorFacturas = new ControladorFacturas();
+    ControladorProductos objControladorProductos = new ControladorProductos();
 
     public boolean generarReporteExcel(List<Modelo.Compra> data, String fileName, String filePath) {
         Workbook book = new XSSFWorkbook();
         Sheet sheet = book.createSheet("productos");
 
         // Crear formato para centrar el contenido
-        CellStyle centeredStyle = book.createCellStyle();
-        centeredStyle.setAlignment(HorizontalAlignment.CENTER);
+        CellStyle centeredWrapStyle = book.createCellStyle();
+        centeredWrapStyle.setAlignment(HorizontalAlignment.CENTER);
+        centeredWrapStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        centeredWrapStyle.setWrapText(true);
 
         // Crear formato para el color de fondo de las celdas
         CellStyle coloredStyle = book.createCellStyle();
-        coloredStyle.cloneStyleFrom(centeredStyle);
+        coloredStyle.cloneStyleFrom(centeredWrapStyle);
         coloredStyle.setFillForegroundColor(IndexedColors.PLUM.getIndex());
         coloredStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
@@ -51,42 +58,92 @@ public class ControladorReportes {
         // Crear formato para las fechas
         CreationHelper creationHelper = book.getCreationHelper();
         CellStyle dateStyle = book.createCellStyle();
-        dateStyle.cloneStyleFrom(centeredStyle);
+        dateStyle.cloneStyleFrom(centeredWrapStyle);
         dateStyle.setDataFormat(creationHelper.createDataFormat().getFormat("dd/MM/yyyy HH:mm:ss"));
 
-        //titulos
-        Row rowTitulo = sheet.createRow(1);
+        // Obtener la fecha actual
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String formattedDate = now.format(formatter);
 
-        String[] titulos = {"Fecha", "Id", "Comentario", "No mesa", "Costo total", "Empleado"};
+        // Escribir la fecha en una celda específica
+        Row dateRow = sheet.createRow(1);
+        Cell dateCell = dateRow.createCell(1);
+        dateCell.setCellValue("Fecha del reporte:");
+        dateCell.setCellStyle(dateStyle);
+
+        Cell valueCell = dateRow.createCell(2);
+        valueCell.setCellValue(formattedDate);
+
+        //titulos
+        Row rowTitulo = sheet.createRow(3);
+
+        String[] titulos = {"Fecha", "Id compra", "Comentario", "No mesa", "Empleado", "Costo total", "", "", "Id compra", "Id producto",
+            "Nombre", "cantidad"};
 
         for (int i = 1; i <= titulos.length; i++) {
-            Cell cell = rowTitulo.createCell(i);
-            cell.setCellValue(titulos[i - 1]);
-            cell.setCellStyle(centeredStyle);
-            cell.setCellStyle(coloredStyle);
-            cell.setCellStyle(textCellStyle);
+            if (!titulos[i - 1].equals("")) {
+                Cell cell = rowTitulo.createCell(i);
+                cell.setCellValue(titulos[i - 1]);
+                cell.setCellStyle(centeredWrapStyle);
+                cell.setCellStyle(coloredStyle);
+                cell.setCellStyle(textCellStyle);
+            }
         }
 
-        int rowNum = 2;
+        int rowNum = 4;
         for (Modelo.Compra compra : data) {
             Modelo.Mesas objMesa = objControladorMesas.getMesa(compra.getMesas_id());
             Modelo.Users objUser = objControladorPedidos.getUsuario(compra.getUsuarios_id());
 
-            Object[] valores = {compra.getFecha_hora(), compra.getIdCompra(), compra.getComentario(), objMesa.getNumero_mesa(), compra.getCosto_total(), objUser.getNombre() + " " + objUser.getApellido()};
+            Object[] valoresCompra = {compra.getFecha_hora(), compra.getIdCompra(), compra.getComentario(), objMesa.getNumero_mesa(),
+                objUser.getNombre() + " " + objUser.getApellido(), compra.getCosto_total()};
 
             Row row = sheet.createRow(rowNum++);
             for (int i = 1; i <= 6; i++) {
                 Cell cell = row.createCell(i);
-                cell.setCellValue(valores[i - 1].toString());
-                cell.setCellStyle(centeredStyle);
-                if(i == 1){
+                cell.setCellValue(valoresCompra[i - 1].toString());
+                cell.setCellStyle(centeredWrapStyle);
+                if (i == 1) {
                     cell.setCellStyle(dateStyle);
                 }
             }
         }
 
-        for (int i = 1; i < 7; i++) {
-            sheet.setColumnWidth(i, 6000);
+        rowNum = 4;
+        for (Modelo.Compra compra : data) {
+            List<Modelo.Factura> objFactura = objControladorFacturas.getFacturas(compra.getIdCompra());
+
+            //recorremos los productos
+            for (int nProductos = 0; nProductos < objFactura.size(); nProductos++) {
+                int contadorCeldaProductos = 9;
+                int productoId = objFactura.get(nProductos).getProductos_id();
+                Modelo.Productos producto = objControladorProductos.getProducto(productoId);
+
+                Object[] valoresProducto = {compra.getIdCompra(), productoId, producto.getNombre(), objFactura.get(nProductos).getCantidad_producto()};
+
+                Row row;
+                if (sheet.getRow(rowNum) == null) {
+                    row = sheet.createRow(rowNum);
+                } else {
+                    row = sheet.getRow(rowNum);
+                }
+
+                //llenar celdas de fila x
+                for (Object valoresProducto1 : valoresProducto) {
+                    Cell cell = row.createCell(contadorCeldaProductos);
+                    cell.setCellValue(valoresProducto1.toString());
+                    cell.setCellStyle(centeredWrapStyle);
+                    contadorCeldaProductos++;
+                }
+                rowNum++;
+            }
+        }
+
+        for (int i = 1; i <= titulos.length; i++) {
+            if (!titulos[i - 1].equals("")) {
+                sheet.setColumnWidth(i, 6000);
+            }
         }
 
         try {
